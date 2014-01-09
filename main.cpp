bda4761fec0567f6ae93beb78e5b6b575e16844b
powerpeanut <github@powerpeanut.de>
2014-01-09 09:47:52 +0100

    Menue

--- main.cpp
+++ main.cpp
@@ -7,36 +7,48 @@
 using namespace cv;
 using namespace std;
 
-string source = "nothing";
-void getSource();
+string getSource();
 
 //#################################################################################
 int main(){
 	Monitor mon;
+	string source = "nothing";
 
-	cout << "######################## Street Stream ########################\n" << endl;
+	///// SWITCH CASE BAUM (if else wegen string) um unterschiedliche Videos zu wählen ...
+	//evtl noch video per pfad wählen lassen (fenster??)
+	//nach video ende oder abbruch des monitos hierhin zurückkehren!!!
+	//dabei alle alten fenster schließen!!
+	while(source != "ende"){    
+		cout << "######################## Street Stream ########################\n" << endl;
 
-	//Quelle wählen
-	getSource();
+		//Quelle wählen
+		cout << "=============== QUELLE WAEHLEN ================" << endl;
+		source = getSource();
 
-	if(source == "0"){
-		mon.process("0"); // 0=Kamera - Pfad für Videodatei
-	}
-	else if(source == "1"){
-		//mon.process("Micro-dance.avi");
-		//mon.process("TestNils2.avi");
+		//Quelle starten
+		if(source == "0"){
+			mon.process("0"); // 0=Kamera - Pfad für Videodatei
+		}
+		else if(source == "1"){
+			//mon.process("Micro-dance.avi");
+			//mon.process("TestNils2.avi");
 
-		//## Kamera sollte am besten senkrecht zur Straße ausgerichtet sein
-		mon.process("Bochum1.avi"); //http://www.youtube.com/watch?v=Jc22qUyuN7k
-		//mon.process("Bochum2.avi"); //http://www.youtube.com/watch?v=ZD1zeSSJZ7k
+			//## Kamera sollte am besten senkrecht zur Straße ausgerichtet sein
+			mon.process("Bochum1.avi"); //http://www.youtube.com/watch?v=Jc22qUyuN7k
+			//mon.process("Bochum2.avi"); //http://www.youtube.com/watch?v=ZD1zeSSJZ7k
+		}
+		else if(source == "ende"){
+			break;
+		}
 	}
 	return 0;
 }
 
 //#################################################################################
-//User Abfrage ob Videodatei oder Webcam gewählt werden soll
-void getSource(){
-	cout << "=============== QUELLE WAEHLEN ================" << endl;
+//User Abfrage, beendet erst, wenn gültige eingabe erkannt
+//ob Videodatei oder Webcam gewählt werden soll (ende = exit)
+string getSource(){
+	string source = "nothing";
 
 	while(true){
 		cout << "Waehle 0 fuer Webcam oder 1 fuer Videodatei: ";
@@ -45,12 +57,15 @@ void getSource(){
 			break;
 		}
 		else if(source == "1"){
-			source = "1";
+			break;
+		}
+		else if(source == "ende"){
 			break;
 		}
 	}
 	cout << "gewaehlt: " << source << endl;
 	destroyAllWindows();
+	return source;
 }
 
 
